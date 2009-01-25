b7e7fb7480a75e448e6cdbe6b901eac8f43f34b6
Dimitrij Denissenko <contact@dvisionfactory.com>
2009-01-25 12:33:34 +0000

    Fixed page renaming in Wiki Added spec

---
+++ extensions/retro_wiki/spec/views/wiki/rename.html.erb_spec.rb
@@ -0,0 +1,26 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe "/wiki/rename.html.erb" do
+  
+  before(:each) do
+    @project = stub_model(Project, :to_param => 'retro')
+    @wiki_page = stub_model(WikiPage, :title => 'New', :title_was => 'Old')
+    Project.stub!(:current).and_return(@project)
+    assigns[:wiki_page] = @wiki_page 
+  end
+
+  def do_render
+    render "wiki/rename.html.erb"
+  end
+    
+  it "should render without errors" do
+    do_render
+  end
+  
+  it "should have a form putting to update-title" do
+    do_render
+    response.should have_form_puting_to(update_title_project_wiki_page_path(@project, 'Old'))
+  end
+
+end
+
