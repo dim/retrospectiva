6239629a8b632f3c3a415774271ab3e25b085469
Dimitrij Denissenko <contact@dvisionfactory.com>
2009-01-27 08:20:48 +0000

    Added merging of permissions

--- lib/retrospectiva/access_manager/secure_controller.rb
+++ lib/retrospectiva/access_manager/secure_controller.rb
@@ -1,11 +1,11 @@
 module Retrospectiva
   module AccessManager
-  
     module SecureController
+
       def self.included(base) #:nodoc:
         base.extend(ClassMethods)
         base.send(:include, InstanceMethods)
-      
+
         [:before_authenticate, :after_authenticate].each do |method|
           base.class_eval <<-END_EVAL
             def self.#{method}(*callbacks, &block)
@@ -13,31 +13,31 @@ module Retrospectiva
               write_inheritable_array(#{method.to_sym.inspect}, callbacks)
             end
           END_EVAL
-        end        
+        end
       end
-    
+
       # == Step-wise action protection
-      # 
+      #
       # This module provides class-level methods for more modular access
       # restriction definitions. Using the authorize filter
       # allows all actions being accessed by supervisor only. The 'require_*'
       # methods can be used to ease this restriction.
-      # 
-      # Available methods: 
+      #
+      # Available methods:
       # * require_permissions
       # * require_user
-      # 
+      #
       # See the documentation of each method for more details.
-      # 
+      #
       module ClassMethods
         # Returns true if the user is logged in and also permitted to access the
         # action (given by the action_name parameter), else false.
-        # 
+        #
         # The before_filter :authorize u.A. uses this method to determine if access
         # should be granted or refused.
         def authorize?(action_name, request_params = {}, user = User.current, project = nil)
           action_name = action_name.to_s
-    
+
           if user.blank?
             false
           elsif user.admin?
@@ -47,147 +47,138 @@ module Retrospectiva
               tokens << { :project => project }
               user.permitted?(*tokens)
             end.uniq == [true]
-          else 
+          else
             require_user.include?(action_name)
           end
         end
-                      
+
         # This restricts action access to admins and users that have a permission
-        # 
+        #
         #     require_permissions :tickets,
         #       :view => ['index', 'show'],
         #       :update => ['edit', 'update']
-        #     
+        #
         def require_permissions(resource = nil, options = {})
-          ensure_authorize_before_filter
-          unless resource.blank?
-            permission_map = options.inject({}) do |result, (permission, actions)|
-              [actions].flatten.map(&:to_s).each do |action| 
-                result[action] ||= []
-                result[action] << [resource.to_sym, permission.to_sym]
+          ensure_authorize_before_filter!
+
+          returning(@retro_am_auth_require_permissions ||= {}) do |permission_map|
+
+            options.each do |permission, actions|
+              [actions].flatten.map(&:to_s).each do |action|
+                permission_map[action] ||= []
+                permission_map[action] << [resource.to_sym, permission.to_sym]
               end
-              result
-            end
-            write_authorization_attribute(:require_permissions, permission_map) 
+            end if resource.present?
+
           end
-          read_authorization_attribute(:require_permissions, {})
         end
-         
+
         # This restricts action access to all logged-in users (also Public). Arguments may contain
         # an arbitrary number of method references. Example:
-        # 
+        #
         #     require_user('list', 'view')
-        #     
+        #
         def require_user(*actions)
-          ensure_authorize_before_filter
-          write_authorization_attribute(:require_user, actions.map(&:to_s)) unless actions.blank?
-          read_authorization_attribute(:require_user, [])
-        end
-          
-        def authorized_controller?
-          before_filters.include?(:authorize) || before_filters.include?('authorize')      
-        end
-        
-        private    
-          def write_authorization_attribute(attribute, value)
-            write_inheritable_attribute("retro-am::authorization::#{attribute}", value)
+          ensure_authorize_before_filter!
+          returning(@retro_am_auth_require_user ||= []) do |permissions|
+            permissions += actions.map(&:to_s) unless actions.blank?
           end
+        end
 
-          def read_authorization_attribute(attribute, default = [])
-            read_inheritable_attribute("retro-am::authorization::#{attribute}") || default
-          end
-            
-          def ensure_authorize_before_filter
-            unless authorized_controller?
-              raise Error, "Unable to find an 'authorize' before_filter."
-            end
+        def authorized_controller?
+          before_filters.include?(:authorize) || before_filters.include?('authorize')
+        end
+
+        private
+
+          def ensure_authorize_before_filter!
+            raise Error, "Unable to find an 'authorize' before_filter." unless authorized_controller?
           end
       end
-   
-      # == Global safety
-      # 
+
+
       # This module provides two ready-made before_filters that can easily be
       # included to secure a controller:
-      # 
+      #
       # * authenticate
       # * authorize
-      # 
+      #
       # See the documentation of each filter for more details.
-      # 
-      module InstanceMethods      
+      #
+      module InstanceMethods
         protected
           # This filter is generally used to protect actions from being accessed
           # by non logged-in users. Example:
-          # 
+          #
           #   class SomeController < ActionController::Base
           #     # You must be logged-in to access this controller's actions
           #     before_filter :authenticate
-          #     
+          #
           def authenticate
             result = security_callback(:before_authenticate)
             return false if result == false
-            
+
             if session[:user_id].is_a?(Integer)
               logger.debug("Authenticating user with id = #{session[:user_id]}") if logger
               User.current = User.find_by_id session[:user_id], :include => {:groups => :projects}
             else
               User.current = User.public_user
-              raise "You have no Public user. See the INSTALL file for instructions on setting up default content." unless User.current            
+              raise "You have no Public user. See the INSTALL file for instructions on setting up default content." unless User.current
             end
-            
+
             if User.current.blank?
               logger.debug("Authentication failed. Redirect to login.") if logger
               reset_session
               redirect_to login_path
-              return false              
+              return false
             end
 
             security_callback(:after_authenticate)
           end
-          
+
           def before_authenticate() end
           def after_authenticate() end
-                      
-          # This filter restricts action access to a specific user-level. Potential 
+
+          # This filter restricts action access to a specific user-level. Potential
           # user levels are:
-          # 
+          #
           # * admin:           access to admins only
           # * permission:      access to admins and those logged-in who have a permission
           # * login:           access to all logged-in users
-          # 
-          # The authorize filter follows a 'deny all then allow some' strategy, i.e. 
-          # primarily, actions will be guarded from beeing accesses by any other than 
-          # admin. This restriction can be reduced stepwise by using require_* 
+          #
+          # The authorize filter follows a 'deny all then allow some' strategy, i.e.
+          # primarily, actions will be guarded from beeing accesses by any other than
+          # admin. This restriction can be reduced stepwise by using require_*
           # class-level definitions, described in next chapter. Example:
-          # 
+          #
           #   class SomeController < ActionController::Base
           #     # You must be supervisor to access this controller's actions
           #     before_filter :authorize
-          # 
+          #
           def authorize
             authenticate unless User.current
-            raise ActionController::UnknownAction unless self.class.action_methods.include?(action_name)    
+            raise ActionController::UnknownAction unless self.class.action_methods.include?(action_name)
             refuse_authorization! unless self.class.authorize?(action_name, params, User.current, Project.current)
             return true
           end
-        
+
           def refuse_authorization!
             raise NoAuthorizationError, "No authorization for action: #{action_name}, params: #{params.inspect}, user: #{User.current.username rescue 'nil'}, project: #{Project.current.name rescue 'nil'}"
           end
-        
+
         private
 
-          def security_callback(method)    
+          def security_callback(method)
             methods = self.class.read_inheritable_attribute(method.to_sym) || []
             methods.each do |callback|
               result = send(callback)
               return false if result == false
-            end    
+            end
             send(method)
           end
-    
-      end        
-    end
 
+      end
+
+    end
   end
 end
