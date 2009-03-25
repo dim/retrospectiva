af78bbe82b86cdd48a6a3f86c8960c39968b9020
Dimitrij Denissenko <dimitrij.denissenko@platform-a.com>
2009-03-25 17:55:51 +0000

    Extended maximum blog comment length

--- extensions/retro_blog/models/blog_comment.rb
+++ extensions/retro_blog/models/blog_comment.rb
@@ -1,5 +1,5 @@
 #--
-# Copyright (C) 2008 Dimitrij Denissenko
+# Copyright (C) 2009 Dimitrij Denissenko
 # Please read LICENSE document for more information.
 #++
 class BlogComment < ActiveRecord::Base
@@ -8,5 +8,5 @@ class BlogComment < ActiveRecord::Base
   validates_presence_of :author, :content
   validates_association_of :blog_post
   validates_as_email :email, :allow_blank => true
-  validates_length_of :content, :maximum => 600
+  validates_length_of :content, :maximum => 6000
 end
