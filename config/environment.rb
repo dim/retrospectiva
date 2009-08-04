5000ec70403a4b28424eea729f7443e3a38e2789
Dimitrij Denissenko <contact@dvisionfactory.com>
2009-08-04 07:30:36 +0100

    Updated version

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file
 
-RETROSPECTIVA_VERSION = '1.9.721'
+RETROSPECTIVA_VERSION = '1.9.804'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
