a0564f670488de7a8d679fca1aec3ddfce5e7355
Dimitrij Denissenko <contact@dvisionfactory.com>
2009-12-02 12:48:33 +0000

    Added configuration templates for various databases. Bumped version.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file
 
-RETROSPECTIVA_VERSION = '1.9.1130'
+RETROSPECTIVA_VERSION = '1.9.1201'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
