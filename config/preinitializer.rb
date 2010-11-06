0a9658b61041787bf7d4abf4fbf1828b9b21b672
Konstantin Shabanov <kes.eclipse@gmail.com>
2010-11-06 15:59:39 +0600

    fix issue with passenger and move some initialization to boot.rb (ticket #858) (based on johnyb and Tony B commits)

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -18,17 +18,3 @@ rescue Bundler::GemNotFound
   raise RuntimeError, "Bundler couldn't find some gems." +
     "Did you run `bundle install`?"
 end
-
-class Rails::Boot
-  def run
-    load_initializer
-
-    Rails::Initializer.class_eval do
-      def load_gems
-        @bundler_loaded ||= Bundler.require :default, Rails.env
-      end
-    end
-
-    Rails::Initializer.run(:set_load_path)
-  end
-end
